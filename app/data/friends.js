39bf4912bdf9963cac29f2731744effec0bc07f3
Judi Perez <j_vperez29@bellsouth.net>
2018-03-02 18:54:06 -0500

    2nd try

---
+++ app/data/friends.js
@@ -0,0 +1,67 @@
+let friends = [
+{
+  "name":"Sue Smith",
+  "photo":"https://thumb9.shutterstock.com/display_pic_with_logo/1595666/230098735/stock-photo-closeup-of-a-mature-woman-smiling-at-the-camera-230098735.jpg",
+  "scores":[
+      1, 2, 3, 4, 5, 5, 3, 3, 4, 2
+    ] 
+},
+{
+  "name":"Jackie Smiles",
+  "photo":"https://thumb9.shutterstock.com/display_pic_with_logo/1595666/163195115/stock-photo-close-up-of-a-woman-smiling-163195115.jpg",
+  "scores":[
+      3, 3, 3, 3, 3, 4, 4, 4, 2, 1
+    ]
+},
+{
+  "name":"Candy Crush",
+  "photo":"https://image.shutterstock.com/z/stock-photo-portrait-of-a-young-beautiful-woman-standing-outside-479836438.jpg",
+  "scores":[
+      1, 1, 1, 2, 2, 3, 3, 4, 5, 3
+    ]
+},
+{
+  "name":"Victor Ramon",
+  "photo":"https://image.shutterstock.com/z/stock-photo-elegant-young-handsome-man-with-a-beard-in-a-white-classic-suit-the-hotel-s-interior-photo-studio-453684553.jpg",
+  "scores":[
+    3, 3, 3, 4, 4, 3, 3, 3, 2, 1
+    ]
+},
+{
+  "name":"Sam Smith",
+  "photo":"https://media.istockphoto.com/photos/african-american-fireman-at-fire-station-picture-id473790400",
+  "scores":[
+     1, 2, 2, 2, 2, 2, 2, 2, 3, 3
+    ]
+},
+{
+  "name":"Timmy Gene",
+  "photo":"https://image.shutterstock.com/z/stock-photo-handsome-young-man-on-grey-background-looking-at-camera-portrait-of-laughing-young-man-with-hands-492562780.jpg",
+  "scores":[
+      1, 2, 2, 3, 3, 4, 4, 5, 4, 3
+    ]
+}, 
+{
+  "name":"Richard Hands",
+  "photo":"https://image.shutterstock.com/z/stock-photo-portrait-of-young-handsome-man-smiling-outdoor-213159280.jpg",
+  "scores":[
+      2, 2, 2, 3, 3, 3, 3, 4, 4, 4
+    ]
+}, 
+{
+  "name":"Matthew Miller",
+  "photo":"https://image.shutterstock.com/z/stock-photo-sexy-closeup-portrait-of-handsome-topless-male-model-with-beautiful-eyes-on-the-beach-black-and-511127908.jpg",
+  "scores":[
+     3, 2, 1, 4, 3, 5, 4, 2, 1, 3 
+    ]
+}, 
+{
+  "name":"Marie Ansley",
+  "photo":"https://image.shutterstock.com/z/stock-photo-close-up-portrait-of-beautiful-older-woman-standing-in-study-smiling-603639869.jpg",
+  "scores":[
+     3, 3, 3, 4, 4, 1, 1, 2, 5, 4
+    ]
+}
+];
+
+module.exports = friends;
