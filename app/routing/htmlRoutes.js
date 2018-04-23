a60960db7e57e934ea990a3851a8f76a4d42370b
jjudi3 <j_vperez29@bellsouth.net>
2018-04-23 15:13:08 -0400

    Add files via upload

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,17 +1,13 @@
-module.exports = (app) => {
+// dependency
+var path = require('path');
 
-	app.get('/', (req, res) => {
-		// res.sendFile(path.join(__dirname, 'home.html'));
-		res.sendFile('home.html', { root: './app/public' });
+module.exports = function(app) {
+	//GET request to display survey page
+	app.get('/survey', function(req, res){
+		res.sendFile(path.join(__dirname, '../public/survey.html'));
+	})
+// If no matching route is found default to home
+	app.use(function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
 	});
-
-	app.get('/survey', (req,res) => {
-		// res.sendFile(path.join(__dirname, 'survey.html'));
-		res.sendFile('survey.html', { root: './app/public' });
-	})
-
-	app.get('/logic.js', (req,res) => {
-		// res.sendFile(path.join(__dirname, 'survey.html'));
-		res.sendFile('logic.js', { root: './' });
-	})
-};
+}
