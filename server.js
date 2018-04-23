00d2d08595462bddaa6591b256fefd65e871c656
jjudi3 <j_vperez29@bellsouth.net>
2018-04-23 15:11:48 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+//Dependencies:
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
+//ROUTER
+require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);
 
-require("./app/routing/htmlRoutes")(app);
-require("./app/routing/apiRoutes")(app);
-
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// Starts the server to begin listening
+app.listen(PORT, function () {
+  console.log('App listening on PORT: ' + PORT);
+});
